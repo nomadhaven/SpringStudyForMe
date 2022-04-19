a55611e32aa1c6faf6564aa3c621173ec1e04468
Nahyun <kimkuji@naver.com>
2022-04-19 18:40:27 +0900

    220419 27.Exception

---
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController2.java
@@ -0,0 +1,40 @@
+package com.fastcampus.ch2;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExceptionController2 {
+	@ExceptionHandler({ NullPointerException.class, FileNotFoundException.class })
+	public String catcher2(Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+
+	
+	 @ExceptionHandler(Exception.class) 
+	 public String catcher(Exception ex, Model m)
+	 { m.addAttribute("ex", ex);
+	 
+	 return "error"; }
+	
+	
+	@RequestMapping("/ex")
+	public String main() throws Exception{
+			throw new Exception("예외가 발생했습니다.");
+	}
+	
+	@RequestMapping("/ex2")
+	public String main2() throws Exception{
+			throw new FileNotFoundException("예외가 발생했습니다.");
+	
+	}
+	
+
+	
+	
+}
