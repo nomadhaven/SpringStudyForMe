69a77a0fc23036659d929bcf8f4f1750f78646e3
Nahyun <kimkuji@naver.com>
2022-04-20 22:34:45 +0900

    220420 28.Exception Practice

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,10 +2,12 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
@@ -19,16 +21,19 @@ public class ExceptionController {
 
 	
 	 @ExceptionHandler(Exception.class) 
+	 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //200 에러를 500으로 바꾼다.
 	 public String catcher(Exception ex, Model m) {
 	 System.out.println("catcher() in ExceptionController");
-		System.out.println("m="+m);
-	 m.addAttribute("ex", ex);
+	 System.out.println("m="+m);
+//	 m.addAttribute("ex", ex);
 	 
-	 return "error"; }
+	 return "error"; 
+	 }
 	
 
 	@RequestMapping("/ex")
 	public String main(Model m) throws Exception {
+		 m.addAttribute("msg", "message from ExceptionConlltoller");
 		throw new Exception("예외가 발생했습니다.");
 	}
 
--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController2.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController2.java
@@ -2,33 +2,32 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+//@ResponseStatus(HttpStatus.BAD_REQUEST) // 500 -> 400 
+class MyException extends RuntimeException {
+	MyException(String msg){
+		super(msg);
+	}
+	MyException(){ this(""); }
+}
+
 
 @Controller
 public class ExceptionController2 {
-	@ExceptionHandler({ NullPointerException.class, FileNotFoundException.class })
-	public String catcher2(Exception ex, Model m) {
-		m.addAttribute("ex", ex);
-		return "error";
-	}
-
 	
-	 @ExceptionHandler(Exception.class) 
-	 public String catcher(Exception ex, Model m)
-	 { m.addAttribute("ex", ex);
-	 
-	 return "error"; }
-	
-	
-	@RequestMapping("/ex")
+	@RequestMapping("/ex3")
 	public String main() throws Exception{
-			throw new Exception("예외가 발생했습니다.");
+			throw new MyException("예외가 발생했습니다.");
 	}
 	
-	@RequestMapping("/ex2")
+	@RequestMapping("/ex4")
 	public String main2() throws Exception{
 			throw new FileNotFoundException("예외가 발생했습니다.");
 	
